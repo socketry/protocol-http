04b97a7fc6d1137935a9acfd9a5981bfeabdda6c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-06-05 18:12:30 +1200

    Improve argument handling.

--- lib/protocol/http/middleware/builder.rb
+++ lib/protocol/http/middleware/builder.rb
@@ -40,8 +40,8 @@ module Protocol
 					@app = default_app
 				end
 				
-				def use(middleware, *arguments, &block)
-					@use << proc {|app| middleware.new(app, *arguments, &block)}
+				def use(middleware, *arguments, **options, &block)
+					@use << proc {|app| middleware.new(app, *arguments, **options, &block)}
 				end
 				
 				ruby2_keywords(:use) if respond_to?(:ruby2_keywords, true)
