7565b957cfdf6fb8f8f81c733b1b42b47b19f497
thomas morgan <tm@iprog.com>
2023-08-23 11:16:11 -0600

    cache-control: add must-revalidate, proxy-revalidate, s-maxage

--- test/protocol/http/header/cache_control.rb
+++ test/protocol/http/header/cache_control.rb
@@ -2,18 +2,29 @@
 
 # Released under the MIT License.
 # Copyright, 2023, by Samuel Williams.
+# Copyright, 2023, by Thomas Morgan.
 
 require 'protocol/http/header/cache_control'
 
 describe Protocol::HTTP::Header::CacheControl do
 	let(:header) {subject.new(description)}
 	
-	with "max-age=60, public" do
+	with "max-age=60, s-maxage=30, public" do
 		it "correctly parses cache header" do
 			expect(header).to have_attributes(
 				public?: be == true,
 				private?: be == false,
 				max_age: be == 60,
+				s_maxage: be == 30,
+			)
+		end
+	end
+	
+	with "max-age=-10, s-maxage=0x22" do
+		it "gracefully handles invalid values" do
+			expect(header).to have_attributes(
+				max_age: be == nil,
+				s_maxage: be == nil,
 			)
 		end
 	end
@@ -47,6 +58,22 @@ describe Protocol::HTTP::Header::CacheControl do
 		it "correctly parses cache header" do
 			expect(header).to have_attributes(
 				streaming?: be == true,
+			)
+		end
+	end
+	
+	with "must-revalidate" do
+		it "correctly parses cache header" do
+			expect(header).to have_attributes(
+				must_revalidate?: be == true,
+			)
+		end
+	end
+	
+	with "proxy-revalidate" do
+		it "correctly parses cache header" do
+			expect(header).to have_attributes(
+				proxy_revalidate?: be == true,
 			)
 		end
 	end
