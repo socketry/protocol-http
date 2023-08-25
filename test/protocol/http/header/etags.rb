22ed9250b384a46060a162f1a5b2bfd266b1b597
zarqman <tm@iprog.com>
2023-08-25 17:25:21 -0600

    Add `#strong_match?` and `#weak_match?` to ETags header. (#40)

--- test/protocol/http/header/etags.rb
+++ test/protocol/http/header/etags.rb
@@ -2,6 +2,7 @@
 
 # Released under the MIT License.
 # Copyright, 2020-2023, by Samuel Williams.
+# Copyright, 2023, by Thomas Morgan.
 
 require 'protocol/http/header/etags'
 
@@ -14,21 +15,43 @@ describe Protocol::HTTP::Header::ETags do
 		end
 		
 		it "matches anything" do
-			expect(header).to be(:match?, "anything")
+			expect(header).to be(:match?, '"anything"')
 		end
 	end
 	
-	with "abcd" do
+	with '"abcd"' do
 		it "is not a wildcard" do
 			expect(header).not.to be(:wildcard?)
 		end
 		
 		it "matches itself" do
-			expect(header).to be(:match?, "abcd")
+			expect(header).to be(:match?, '"abcd"')
+		end
+		
+		it "strongly matches only another strong etag" do
+			expect(header).to be(:strong_match?, '"abcd"')
+			expect(header).not.to be(:strong_match?, 'W/"abcd"')
+		end
+		
+		it "weakly matches both weak and strong etags" do
+			expect(header).to be(:weak_match?, '"abcd"')
+			expect(header).to be(:weak_match?, 'W/"abcd"')
 		end
 		
 		it "does not match anything else" do
-			expect(header).not.to be(:match?, "anything else")
+			expect(header).not.to be(:match?, '"anything else"')
+		end
+	end
+	
+	with 'W/"abcd"' do
+		it "never strongly matches" do
+			expect(header).not.to be(:strong_match?, '"abcd"')
+			expect(header).not.to be(:strong_match?, 'W/"abcd"')
+		end
+		
+		it "weakly matches both weak and strong etags" do
+			expect(header).to be(:weak_match?, '"abcd"')
+			expect(header).to be(:weak_match?, 'W/"abcd"')
 		end
 	end
 end
