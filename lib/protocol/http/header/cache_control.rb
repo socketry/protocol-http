6c50b9a455a2e7ec34d277121fbf949902b21cdd
zarqman <tm@iprog.com>
2023-08-25 18:35:58 -0600

    cache-control: add must-revalidate, proxy-revalidate, s-maxage (#39)

--- lib/protocol/http/header/cache_control.rb
+++ lib/protocol/http/header/cache_control.rb
@@ -2,6 +2,7 @@
 
 # Released under the MIT License.
 # Copyright, 2020-2023, by Samuel Williams.
+# Copyright, 2023, by Thomas Morgan.
 
 require_relative 'split'
 
@@ -14,10 +15,14 @@ module Protocol
 				NO_CACHE = 'no-cache'
 				NO_STORE = 'no-store'
 				MAX_AGE = 'max-age'
+				S_MAXAGE = 's-maxage'
 				
 				STATIC = 'static'
 				DYNAMIC = 'dynamic'
 				STREAMING = 'streaming'
+				
+				MUST_REVALIDATE = 'must-revalidate'
+				PROXY_REVALIDATE = 'proxy-revalidate'
 				
 				def initialize(value = nil)
 					super(value&.downcase)
@@ -55,11 +60,40 @@ module Protocol
 					self.include?(NO_STORE)
 				end
 				
+				# Indicates that a response must not be used once it is stale.
+				# See https://www.rfc-editor.org/rfc/rfc9111.html#name-must-revalidate
+				def must_revalidate?
+					self.include?(MUST_REVALIDATE)
+				end
+				
+				# Like must-revalidate, but for shared caches only.
+				# See https://www.rfc-editor.org/rfc/rfc9111.html#name-proxy-revalidate
+				def proxy_revalidate?
+					self.include?(PROXY_REVALIDATE)
+				end
+				
+				# The maximum time, in seconds, a response should be considered fresh.
+				# See https://www.rfc-editor.org/rfc/rfc9111.html#name-max-age-2
 				def max_age
-					if value = self.find{|value| value.start_with?(MAX_AGE)}
+					find_integer_value(MAX_AGE)
+				end
+				
+				# Like max-age, but for shared caches only, which should use it before
+				# max-age when present.
+				# See https://www.rfc-editor.org/rfc/rfc9111.html#name-s-maxage
+				def s_maxage
+					find_integer_value(S_MAXAGE)
+				end
+				
+				private
+				
+				def find_integer_value(value_name)
+					if value = self.find{|value| value.start_with?(value_name)}
 						_, age = value.split('=', 2)
 						
-						return Integer(age)
+						if age =~ /\A[0-9]+\z/
+							return Integer(age)
+						end
 					end
 				end
 			end
